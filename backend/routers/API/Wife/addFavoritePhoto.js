3c3f8a1003371d3d9ec919222ab9f5825d6de0cd
Kushnir E <kyshnirxeneea@gmail.com>
2023-01-23 10:10:08 +0200

    refactor(fe): changed views pages

--- backend/routers/API/Wife/addFavoritePhoto.js
+++ backend/routers/API/Wife/addFavoritePhoto.js
@@ -1,20 +1,18 @@
-const Router = require('express');
+const Router = require("express");
 const router = Router();
 
-const db = require('../../../service/fireStoreService');
+const db = require("../../../service/fireStoreService");
 
-router.put('/addFavoritePhoto' ,async (req, res) => {
-    try {
-        const {uid, bigLink, link} = req.body;
+router.put("/addFavoritePhoto", async (req, res) => {
+  try {
+    const { uid, bigLink, link } = req.body;
 
-        await db.addUserFavoriteItem({bigLink, link}, uid);
-    
-        res.status(200).send(JSON.stringify({message: 'image save'}))
-    } catch (error) {
-        res.status(500).send(JSON.stringify({message: 'server error: save favorite image'}))
-    }
+    await db.addUserFavoriteItem({ bigLink, link }, uid);
 
+    res.status(200).send(JSON.stringify({ message: "image saved" }));
+  } catch (error) {
+    res.status(500).send(JSON.stringify({ message: "server error: save favorite image" }));
+  }
 });
 
-
-module.exports = router
+module.exports = router;
