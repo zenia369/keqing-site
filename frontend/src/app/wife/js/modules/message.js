40114f3332bf57a08afb7fe8ba0a65cb6e80dc9f
Kushnir E <kyshnirxeneea@gmail.com>
2023-02-13 12:37:33 +0200

    refactor(fe): added import for message module

--- frontend/src/app/wife/js/modules/message.js
+++ frontend/src/app/wife/js/modules/message.js
@@ -1,18 +1,20 @@
+import Toastify from 'toastify-js'
+
 const colors = {
-  200: "linear-gradient(to right, #00b09b, #96c93d)",
-  error: "linear-gradient(to right, #ff5f6d, #ffc371)",
-};
+  200: 'linear-gradient(to right, #00b09b, #96c93d)',
+  error: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+}
 
 export default (status, message) => {
   Toastify({
     text: message,
     duration: 5000,
     close: true,
-    gravity: "bottom",
-    position: "center",
+    gravity: 'bottom',
+    position: 'center',
     stopOnFocus: true,
     style: {
-      background: colors[status] ?? colors["error"],
+      background: colors[status] ?? colors.error,
     },
-  }).showToast();
-};
+  }).showToast()
+}
