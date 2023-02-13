34820867acf7a2ebcabb41c81587bba3c767d999
Kushnir E <kyshnirxeneea@gmail.com>
2023-02-13 16:36:21 +0200

    refactor(be): a little changes in paths file

--- backend/paths.js
+++ backend/paths.js
@@ -5,16 +5,16 @@ const fs = require("fs");
 const BUILD_PATH = path.resolve("/etc/secrets/");
 
 module.exports = {
-  APP_SERVICE_ACCOUNT: fs.existsSync(__dirname, "./data/serviceAccountKey.js")
+  APP_SERVICE_ACCOUNT: fs.existsSync("./data/serviceAccountKey.js")
     ? path.resolve("./data/serviceAccountKey.js")
     : path.join(BUILD_PATH, "serviceAccountKey.js"),
-  APP_PICTURES_DATA: fs.existsSync(__dirname, "./data/pictures_data.json")
+  APP_PICTURES_DATA: fs.existsSync("./data/pictures_data.json")
     ? path.resolve("./data/pictures_data.json")
     : path.join(BUILD_PATH, "pictures_data.json"),
-  APP_CHARACTERS_LIST: fs.existsSync(__dirname, "./data/charactersList.js")
+  APP_CHARACTERS_LIST: fs.existsSync("./data/charactersList.js")
     ? path.resolve("./data/charactersList.js")
     : path.join(BUILD_PATH, "charactersList.js"),
-  APP_FIREBASE_CONFIG: fs.existsSync(__dirname, "./data/firebaseConfig.json")
+  APP_FIREBASE_CONFIG: fs.existsSync("./data/firebaseConfig.json")
     ? path.resolve("./data/firebaseConfig.json")
     : path.join(BUILD_PATH, "firebaseConfig.json"),
 };
