d15bd74852de1f162d37d023f99d65fee36afc77
Kushnir E <kyshnirxeneea@gmail.com>
2023-02-28 10:31:55 +0200

    feat(be): added linting

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './jsconfig.json',
+  },
+  plugins: ['prettier', 'eslint-plugin-import'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+}
